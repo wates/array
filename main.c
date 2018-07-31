ee6055258892e30cf5162abf62c25bdb7dc326e9
wates <wate@wate.jp>
2018-08-01 02:12:25 +0900

    basic

--- main.c
+++ main.c
@@ -16,7 +16,9 @@ int main(int argc, char *argv[])
     //##### ここから書き換えてファイルを読み込んで、結果を出力する
     char str[1000];
     fgets(str, sizeof(str), fp);
-    printf("%s", str);
+    int c = 0, *plus = (int*)malloc(atoi(str) * sizeof(int));
+    while(fgets(str, sizeof(str), fp)) atoi(str) < 0 ? printf("%d\n", plus[-atoi(str)-1]) : ( plus[c++] = atoi(str));
+    free(plus);
 
     //##### だいたいここまで書き換える
 
